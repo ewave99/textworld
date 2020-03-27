effbf2f04f48d5e83825d5d99de6931fd7716fe0
ewave99 <ewave9@gmail.com>
2020-03-27 18:01:51 +0000

    Dynamic char length and line count when window resized

--- main.js
+++ main.js
@@ -5,42 +5,27 @@ String.prototype.replaceAt = function(index, replacement) {
 const DISPLAY = document.getElementById('display');
 const TEXTSIZE = 11;
 document.body.style.fontSize = TEXTSIZE.toString() + 'px';
-let CHARS_ACROSS;
-let CHARS_DOWN;
-let lines;
+let CHARS_ACROSS, CHARS_DOWN;
+let CHAR_WIDTH, CHAR_HEIGHT;
+let lines = [];
+
+setCharDimensions();
 
 window.onresize = function () {
-  setCharsAcross();
-  setCharsDown();
-  setLines();
+  setDimensions();
 }
 
-function setCharsAcross() {
-  let ruler = document.getElementById('ruler');
-  let start = Math.floor(window.innerWidth / TEXTSIZE);
-  ruler.textContent = 'x'.repeat(start);
-  let charwidth = ruler.offsetWidth / start;
-  CHARS_ACROSS = Math.floor(window.innerWidth / charwidth)-2;
-  ruler.textContent = '';
+function setCharDimensions() {
+  let startwidth = Math.floor(window.innerWidth / TEXTSIZE);
+  let startheight = Math.floor(window.innerHeight / TEXTSIZE);
+  DISPLAY.textContent = ('.'.repeat(startwidth)+'\n').repeat(startheight);
+  CHAR_WIDTH = DISPLAY.offsetWidth / startwidth;
+  CHAR_HEIGHT = DISPLAY.offsetHeight / startheight;
+  DISPLAY.textContent = '';
 }
 
-function setCharsDown() {
-  let ruler = document.getElementById('ruler');
-  let start = Math.floor(window.innerHeight / TEXTSIZE);
-  ruler.innerText = 'x\n'.repeat(start);
-  let charheight = ruler.offsetHeight / start;
-  CHARS_DOWN = Math.floor(window.innerWidth / charheight);
-  ruler.textContent = '';
-}
-
-function setLines() {
-  for (var i = 0; i < lines.length; i++) {
-    lines[i].innerHTML =  // change to delete node next time
-  }
-  lines = [];
-  for (var i = 0; i < CHARS_DOWN; i++) {
-    lines.push(DISPLAY.appendChild(document.createElement('SPAN')));
-    DISPLAY.appendChild(document.createElement('BR'));
-  }
-  lines[0].textContent = '*'.repeat(CHARS_ACROSS);
+function setDimensions() {
+  CHARS_ACROSS = Math.floor(window.innerWidth / CHAR_WIDTH )-2;
+  CHARS_DOWN = Math.floor(window.innerHeight / CHAR_HEIGHT)-2;
+  DISPLAY.textContent = ('x'.repeat(CHARS_ACROSS)+'\n').repeat(CHARS_DOWN);
 }
