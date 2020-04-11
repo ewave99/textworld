f99e1c109013f01069ff24388d75ccb118d1322a
ewave99 <ewave9@gmail.com>
2020-04-11 15:55:38 +0100

    optimize displaying of shapes a bit  more

--- display/graphics.js
+++ display/graphics.js
@@ -3,19 +3,15 @@
 const COS_TABLE = _.range(360).map(i => Math.cos(i * Math.PI / 180));
 const SIN_TABLE = _.range(360).map(i => Math.sin(i * Math.PI / 180));
 
-// Useful if you want to plot a single point without typing
-//  updateDisplay() each time
-function point(x, y, fillvalue=1) {
-  plot(x, y, fillvalue);
-  updateDisplay();
-}
-
-function plot(x, y, fillvalue=1) {
+function plot(x, y, fillvalue=1, update=true) {
   let within_bound_x = (x >= 0 && x < CHARS_ACROSS);
   let within_bound_y = (y >= 0 && y < CHARS_DOWN);
   if (within_bound_x && within_bound_y) {
     let index = y * CHARS_ACROSS + x;
     CHARS[index] = fillvalue;
+  }
+  if (update) {
+    updateDisplay();
   }
 }
 
@@ -23,18 +19,30 @@ function ellipse(x, y, w, h, { // The syntax here allows keyword arguments
   stroke = true,
   strokevalue = 1,
   fill = false,
-  fillvalue = 1
+  fillvalue = 1,
+  update = true
 } = {}) {
   // If the width is more than 115 then the outline starts to 'crack'
-  //   as the amgles aren't precise enough.
+  //   as the angles aren't precise enough.
   // Could be fixed by drawing line segments between each point if the width
   //   exceeds 115.
   if (stroke) {
-    let px, py;
-    for (var i = 0; i < 360; i++) {
-      px = Math.round(COS_TABLE[i] * w + x);
-      py = Math.round(SIN_TABLE[i] * h + y);
-      plot(px, py, strokevalue);
+    if (w >= 57) {
+      let p1x, p1y, p2x, p2y;
+      for (var i = 0; i < 360; i++) {
+        p1x = Math.round(COS_TABLE[i] * w + x);
+        p1y = Math.round(SIN_TABLE[i] * h + y);
+        p2x = Math.round(COS_TABLE[(i+1)%360] * w + x);
+        p2y = Math.round(SIN_TABLE[(i+1)%360] * h + y);
+        line(p1x, p1y, p2x, p2y, strokevalue, false)
+      }
+    } else {
+      let px, py;
+      for (var i = 0; i < 360; i++) {
+        px = Math.round(COS_TABLE[i] * w + x);
+        py = Math.round(SIN_TABLE[i] * h + y);
+        plot(px, py, strokevalue, false);
+      }
     }
   }
   if (fill) {
@@ -45,7 +53,7 @@ function ellipse(x, y, w, h, { // The syntax here allows keyword arguments
     let x0 = w;
     let dx = 0;
     for (var px = 1-w; px <= w-1; px++) {
-      plot(px+x, y, fillvalue);
+      plot(px+x, y, fillvalue, false);
     }
     for (var py = 1; py < h; py++) {
       let x1 = x0 - (dx - 1);
@@ -55,37 +63,42 @@ function ellipse(x, y, w, h, { // The syntax here allows keyword arguments
       dx = x0 - x1;
       x0 = x1;
       for (var px = 1-x0; px <= x0-1; px++) {
-        plot(px+x, -py+y, fillvalue);
-        plot(px+x, py+y, fillvalue);
+        plot(px+x, -py+y, fillvalue, false);
+        plot(px+x, py+y, fillvalue, false);
       }
     }
   }
-  updateDisplay();
+  if (update) {
+    updateDisplay();
+  }
 }
 
 function rect(x, y, w, h, {
   stroke = true,
   strokevalue = 1,
   fill = false,
-  fillvalue = 1
+  fillvalue = 1,
+  update = true
 } = {}) {
   if (fill) {
     for (var j = 1; j < h; j++) {
       for (var i = 1; i < w; i++) {
-        plot(i+x, j+y, fillvalue);
+        plot(i+x, j+y, fillvalue, false);
       }
     }
   }
   if (stroke) {
-    line(x, y, x+w, y, strokevalue);
-    line(x+w, y, x+w, y+h, strokevalue);
-    line(x+w, y+h, x, y+h, strokevalue);
-    line(x, y+h, x, y, strokevalue);
+    line(x, y, x+w, y, strokevalue, false);
+    line(x+w, y, x+w, y+h, strokevalue, false);
+    line(x+w, y+h, x, y+h, strokevalue, false);
+    line(x, y+h, x, y, strokevalue, false);
   }
-  updateDisplay();
+  if (update) {
+    updateDisplay();
+  }
 }
 
-function arc(x, y, w, h, start, end, strokevalue=1) {
+function arc(x, y, w, h, start, end, strokevalue=1, update=true) {
   // Uses the ellipse algorithm but only draws part of the outline
   let px, py;
   if (start > end) {
@@ -94,12 +107,14 @@ function arc(x, y, w, h, start, end, strokevalue=1) {
   for (var i = start; i <= end; i++) {
     px = Math.round(COS_TABLE[i % 360] * w + x);
     py = Math.round(SIN_TABLE[i % 360] * h + y);
-    plot(px, py, strokevalue);
+    plot(px, py, strokevalue, false);
   }
-  updateDisplay();
+  if (update) {
+    updateDisplay();
+  }
 }
 
-function line(x0, y0, x1, y1, strokevalue=1) {
+function line(x0, y0, x1, y1, strokevalue=1, update=true) {
   let dx = Math.abs(x1-x0);
   let dy = -Math.abs(y1-y0);
   // Optimize performance when drawing vertical / horizontal lines
@@ -110,7 +125,7 @@ function line(x0, y0, x1, y1, strokevalue=1) {
       x0 = temp;
     }
     for (var i = x0; i <= x1; i++) {
-      plot(i, y0, strokevalue);
+      plot(i, y0, strokevalue, false);
     }
   } else if (dx == 0) {
     if (y0 > y1) {
@@ -119,14 +134,14 @@ function line(x0, y0, x1, y1, strokevalue=1) {
       y0 = temp;
     }
     for (var i = y0; i <= y1; i++) {
-      plot(x0, i, strokevalue);
+      plot(x0, i, strokevalue, false);
     }
   } else { // BRESENHAM'S LINE ALGORITHM
     let sx = (x0 < x1) ? 1 : -1;
     let sy = (y0 < y1) ? 1 : -1;
     let err = dx + dy;
     while (x0 != x1 || y0 != y1) {
-      plot(x0, y0, strokevalue);
+      plot(x0, y0, strokevalue, false);
       let e2 = 2 * err;
       if (e2 >= dy) {
         err += dy;
@@ -137,7 +152,9 @@ function line(x0, y0, x1, y1, strokevalue=1) {
         y0 += sy
       }
     }
-    plot(x0, y0, strokevalue);
+    plot(x0, y0, strokevalue, false);
   }
-  updateDisplay();
+  if (update) {
+    updateDisplay();
+  }
 }
