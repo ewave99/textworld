4715c67ce1383efdc4de6d114ad7c0f1444d2522
ewave99 <ewave9@gmail.com>
2020-04-11 15:59:00 +0100

    more optimization

--- main.js
+++ main.js
@@ -1,35 +1,8 @@
-/*
-DEMONSTRATION WITH A SIMPLE BOUNCING BALL
-*/
-
-let balldims = [4, 2];
-let ballpos;
-let speed = 1;
-
-// const movements = {
-//   'ArrowRight': () => ballpos[0]+=2,
-//   'ArrowLeft': () => ballpos[0]-=2,
-//   'ArrowUp': () => ballpos[1]--,
-//   'ArrowDown': () => ballpos[1]++
-// }
-
-function setup() {
-  ballpos = [Math.round(CHARS_ACROSS/2), Math.round(CHARS_DOWN/2)];
-  setInterval(draw, 1000/FPS);
-}
-
 function draw() {
   CHARS = [];
-  ballpos[0] += speed;
-  if (ballpos[0]+balldims[0]+1 >= CHARS_ACROSS || ballpos[0] <= 0) {
-    speed = -speed;
-  }
-  rect(...ballpos, ...balldims, {fill:true});
+  let dims = [
+    Math.round(CHARS_ACROSS/2), Math.round(CHARS_DOWN/2),
+    Math.round(CHARS_ACROSS/2-2), Math.round(CHARS_DOWN/2),
+  ]
+  ellipse(...dims, {fill:true});
 }
-
-// document.onkeydown = function(e) {
-//   if (Object.keys(movements).includes(e.code)) {
-//     movements[e.code]();
-//     draw();
-//   }
-// }
